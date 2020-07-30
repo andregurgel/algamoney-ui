e511e3abcacf11ebb2785c37e0ee4976d1e578e3
Andre <andreggurgelufrn@gmail.com>
2020-07-30 15:38:12 -0300

    Adicionando paginação na tabela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,9 @@ export class AppComponent {
   lancamentos = [
     { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
     { tipo: 'DESPESA', descricao: 'Minecraft', dataVencimento: '30/06/2017', dataPagamento: '30/07/2017', valor: 99.90, pessoa: 'Nuuvem' },
-    { tipo: 'RECEITA', descricao: 'Salario', dataVencimento: '03/07/2017', dataPagamento: null, valor: 500, pessoa: 'Tigitale' }
+    { tipo: 'RECEITA', descricao: 'Salario', dataVencimento: '03/07/2017', dataPagamento: null, valor: 500, pessoa: 'Tigitale' },
+    { tipo: 'RECEITA', descricao: 'Mesada', dataVencimento: '30/06/2017', dataPagamento: null, valor: 150, pessoa: 'Pais' },
+    { tipo: 'DESPESA', descricao: 'Bola de Futebol', dataVencimento: '30/06/2017', dataPagamento: '30/07/2017', valor: 25.90, pessoa: 'Mercadinho' },
+    { tipo: 'RECEITA', descricao: 'Trade', dataVencimento: '03/07/2017', dataPagamento: null, valor: 30.59, pessoa: 'Clear' }
   ];
 }
