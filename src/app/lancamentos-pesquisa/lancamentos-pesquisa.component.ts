3c513d335829b8ac1cd5ecefe4f27beea7a24a39
Andre <andreggurgelufrn@gmail.com>
2020-08-03 15:23:12 -0300

    Implementando menu lateral.

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,9 +8,9 @@ import { Component} from '@angular/core';
 export class LancamentosPesquisaComponent {
   lancamentos = [
     { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
-    { tipo: 'DESPESA', descricao: 'Minecraft', dataVencimento: '30/06/2017', dataPagamento: '30/07/2017', valor: 99.90, pessoa: 'Nuuvem' },
-    { tipo: 'RECEITA', descricao: 'Salario', dataVencimento: '03/07/2017', dataPagamento: null, valor: 500, pessoa: 'Tigitale' },
-    { tipo: 'RECEITA', descricao: 'Mesada', dataVencimento: '30/06/2017', dataPagamento: null, valor: 150, pessoa: 'Pais' },
+    { tipo: 'DESPESA', descricao: 'Jogos', dataVencimento: '30/06/2017', dataPagamento: '30/07/2017', valor: 99.90, pessoa: 'Nuuvem' },
+    { tipo: 'RECEITA', descricao: 'Salario', dataVencimento: '03/07/2017', dataPagamento: null, valor: 500, pessoa: 'Coca-Cola' },
+    { tipo: 'RECEITA', descricao: 'Mesada', dataVencimento: '30/06/2017', dataPagamento: null, valor: 150, pessoa: 'Governo' },
     { tipo: 'DESPESA', descricao: 'Bola de Futebol', dataVencimento: '30/06/2017', dataPagamento: '30/07/2017', valor: 25.90, pessoa: 'Mercadinho' },
     { tipo: 'RECEITA', descricao: 'Trade', dataVencimento: '03/07/2017', dataPagamento: null, valor: 30.59, pessoa: 'Clear' }
   ];
