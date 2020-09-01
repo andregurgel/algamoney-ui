a5ee904fb21b66237a3e2571f6d2cc2ac7714a84
Andre Gurgel <andreggurgelufrn@gmail.com>
2020-09-01 08:09:25 -0300

    Remodulando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,29 @@ import {LOCALE_ID, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 import {registerLocaleData} from "@angular/common";
 import localeBr from "@angular/common/locales/pt";
 import localeBrExtra from "@angular/common/locales/extra/pt";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {LancamentosModule} from "./lancamentos/lancamentos.module";
 import {PessoasModule} from "./pessoas/pessoas.module";
+import {CoreModule} from "./core/core.module";
 
 registerLocaleData(localeBr, 'pt', localeBrExtra);
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     LancamentosModule,
-    PessoasModule
+    PessoasModule,
+    CoreModule
   ],
   providers: [
     {
