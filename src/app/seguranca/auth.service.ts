c2ed67126797004b3ad1a58c7f544cc84694f0b5
Andre <andreggurgelufrn@gmail.com>
2020-10-13 11:37:22 -0300

    Refatorando login e exibindo menu conforme permissoes do user.

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AuthService {
 
   oauthTokenURL = 'http://localhost:8080/oauth/token';
+  token: any;
+  jwtPayload: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private jwtHelperService: JwtHelperService)
+  {
+    this.carregarToken();
+  }
 
   login(usuario: string, senha: string): Promise<void> {
     let headers = new HttpHeaders();
@@ -17,6 +24,44 @@ export class AuthService {
 
     const body = `username=${usuario}&password=${senha}&grant_type=password`;
 
+    return this.http.post<void>(`${this.oauthTokenURL}`, body, { headers, withCredentials: true }).toPromise()
+      .then(response => {
+        this.token = response;
+        this.armazenarToken(this.token.access_token);
+      })
+      .catch(response => {
+        if (response.status === 400) {
+          if (response.error.error === 'invalid_grant') {
+            return Promise.reject('Usuario ou senha inválidos!');
+          }
+        }
+        return Promise.reject(response.error);
+      });
+  }
+
+  armazenarToken(token: string) {
+    this.jwtPayload = this.jwtHelperService.decodeToken(token);
+    localStorage.setItem('access_token', token);
+  }
+
+  carregarToken() {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      this.armazenarToken(token);
+    }
+  }
+
+  getNewAccessToken(): Promise<void> {
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers = headers.append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEByMA==');
+
+    const body = `grant_type=refresh_token`;
+
     return this.http.post<void>(`${this.oauthTokenURL}`, body, { headers, withCredentials: true }).toPromise();
+  }
+
+  temPermissao(permissao: string) {
+    return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
   }
 }
--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -11,43 +11,21 @@ import { Router } from '@angular/router';
 })
 export class LoginFormComponent implements OnInit {
 
-  token: any;
-  jwtPayload: any;
-
   constructor(private authService: AuthService,
               private error: ErrorHandlerService,
               private jwtHelperService: JwtHelperService,
               private router: Router) { }
 
   ngOnInit(): void {
-    this.carregarToken();
   }
 
-  async login(usuario: string, senha: string) {
-    try {
-      this.token = await this.authService.login(usuario, senha);
-      this.armazenarToken(this.token.access_token);
-      await this.router.navigate(['/lancamentos']);
-    } catch (e) {
-      const erro = e.error;
-      if (erro.error === 'invalid_grant') {
-        this.error.handle('Usuário ou senha inválidos');
-      } else {
-        this.error.handle(e);
-      }
-    }
+  login(usuario: string, senha: string) {
+    this.authService.login(usuario, senha)
+      .then(() => {
+        this.router.navigate(['/lancamentos']);
+      })
+      .catch(erro => {
+        this.error.handle(erro);
+      });
   }
-
-  armazenarToken(token: string) {
-    this.jwtPayload = this.jwtHelperService.decodeToken(token);
-    localStorage.setItem('access_token', token);
-  }
-
-  carregarToken() {
-    const token = localStorage.getItem('access_token');
-    if (token) {
-      this.armazenarToken(token);
-    }
-  }
-
 }
